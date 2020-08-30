495191e418074bdde473315eb54398bb79e87612
Bob Wang <wangziling1000@gmail.com>
2020-08-31 00:40:52 +0200

    fix: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-
+import './css/tailwind.css'
 export default (props:any) => {
     return(
         <>
+          <button className='bg-red-500'>Click me</button>
           React Template
         </>
     )
